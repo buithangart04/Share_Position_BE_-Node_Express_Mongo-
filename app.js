d806dcb44604595a8725727b3c8f6491f58a17dc
Bui Thang <thangbuiduc2000@gmail.com>
2021-12-04 20:22:11 +0700

    fix port heroku deployment

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.abumt.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT||5000);
   })
   .catch((error) => {
     console.log(error);
